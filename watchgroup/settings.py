3fd6fc89a051bbd15dd4f9aff46144ab259d773b
PdoyleC <>
2023-10-14 12:25:55 +0000

    updated settings for deploy

--- watchgroup/settings.py
+++ watchgroup/settings.py
@@ -18,19 +18,19 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['watch-blog-1e2436fde037.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["watch-blog-1e2436fde037.herokuapp.com", "localhost",]
 
 
 # Application definition
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'watchgroup.wsgi.application'
+WSGI_APPLICATION = "watchgroup.wsgi.application"
 
 
 # Database
@@ -91,9 +91,8 @@ WSGI_APPLICATION = 'watchgroup.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
@@ -134,10 +133,14 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+
 STATICFILES_STORAGE = (
     "cloudinary_storage.storage.StaticHashedCloudinaryStorage")
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = "/media/"
 
