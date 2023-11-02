c6fe99bb6fcef78ab53fbd178836dd849f459c0b
PdoyleC <>
2023-11-02 15:35:04 +0000

    added about page .html and urls

--- wblog/urls.py
+++ wblog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path(
+        'about',
+        views.generic.TemplateView.as_view(template_name='about.html'),
+        name='about'),
 ]
