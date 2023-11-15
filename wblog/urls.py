c924eb58c818ebd004d6f2fcfd8eea91cc8b5126
PdoyleC <>
2023-11-15 17:27:57 +0000

    edited url for wrtie a post page to open

--- wblog/urls.py
+++ wblog/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path('new_post/', views.NewPost.as_view(), name='new_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path(
         'about',
         views.generic.TemplateView.as_view(template_name='about.html'),
         name='about'),
-    path('new_post/', views.NewPost.as_view(), name='new_post'),
 ]
