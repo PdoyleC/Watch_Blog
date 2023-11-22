c0f94ba670bb6f4763b5c7735e6c6373e4ebe3fa
PdoyleC <>
2023-11-22 09:28:23 +0000

    deployment commit

--- watchgroup/settings.py
+++ watchgroup/settings.py
@@ -30,7 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 ALLOWED_HOSTS = ['watch-blog-1e2436fde037.herokuapp.com', 'localhost',
                  '8000-pdoylec-watch-blog-yifumhml4a.us2.codeanyapp.com']
